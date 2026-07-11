33666ba5d6c0f914d8b6415da8e80ac5737ad754
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-11 13:04:12 +0530

    Concatenation of string

---
+++ Band_NameGen.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band name generator!!!")
+city= input("Enter the city you live in: \n")
+pet= input("Enter your pet name: \n")
+
+print("Your Band name could be: ", city + " " + pet)
