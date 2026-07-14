a8c60c2ec8b095c2a378f36cd3e3a62da8cf9fac
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-14 07:01:22 +0530

    Used randint instead of choice

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,31 @@
+import random
+
+
+User_choice= int(input("Enter your move (0 for rock, 1 for paper and 2 for scissors): \n"))
+
+if User_choice>=3 or User_choice<0:
+    print("Enter valid number")
+
+else:
+    print("Users choice!!")
+
+    computer_choice= random.randint(0,2)
+    print("Computers Choice!!")
+    
+    if User_choice==computer_choice:
+        print("It's a draw!!")
+    elif User_choice==0 and computer_choice==1:
+        print("You lose!!")
+    elif User_choice==0 and computer_choice==2:
+        print("You won!!")
+    elif User_choice==1 and computer_choice==0:
+        print("You won!!")
+    elif User_choice==1 and computer_choice==2:
+        print("You lose!!")
+    elif User_choice==2 and computer_choice==0:
+        print("You lose!!")
+    elif User_choice==2 and computer_choice==1:
+        print("You Won!!")
+
+
+
