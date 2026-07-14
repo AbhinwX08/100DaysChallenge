bd42397449ff835cde4e7cad028aaeb8adff432a
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-14 16:52:45 +0530

    worked on the overflow case and used functions

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -1,15 +1,25 @@
-alphabet_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+             'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+             's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def encrypt(orignal_text, shift_amount):
-    direction= input("Type 'encode' to encrypt and type 'decode' to decrypt: \n").lower()
-
     coded=[]
-    for char in range(len(text)):
-        text[char]=alphabet_lower[char+2]
-        coded.append(text[char])
 
-    print(f"The encypted message of your text is: {coded}")
+    for char in orignal_text:
+        position= alphabet.index(char)
+        new_position= position + shift_amount
+        new_letter= alphabet[new_position]
+
+        coded.append(new_letter)
+
+    print(f"The encypted message of your text is: {''.join(coded)}")
+
+
+direction= input("Type 'encode' to encrypt and type 'decode' to decrypt: \n").lower()
+
 text= input("Enter your text: \n").lower()
-shift= int(input("Type the number you wanted to shift the alphabets by: \n"))
+shift= int(input("Type the number you wanted to shift by: \n"))
 
-encrypt(text,shift)
+encrypt(text, shift)
