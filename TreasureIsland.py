e5f6d1cc34284921845499441cc7a2d61431f7f0
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-15 21:24:21 +0530

    Added new line

--- TreasureIsland.py
+++ TreasureIsland.py
@@ -1,23 +1,23 @@
 print("Welcome to the game of treasure island!!!")
-path= input("Which path would you like to take to reach the island? (e.g: left or right): ").lower()
+path= input("Which path would you like to take to reach the island? (e.g: left or right): \n").lower()
 
 if path=="right":
-    print("You have taken a wrong turn and fell into the Bermuda Triangle")
+    print("You have taken a wrong turn and fell into the Bermuda Triangle\n")
 else:
-    print("You successfully escaped the Right route threat....")
+    print("You successfully escaped the Right route threat....\n")
 
-    choice= input("Would you like to swim or take a boat to reach the the treasure island!!").lower()
+    choice= input("Would you like to swim or take a boat to reach the the treasure island!!\n").lower()
 
     if choice=="swim":
-        print("You ran out of energy!!!! Game lost")
+        print("You ran out of energy!!!! Game lost\n")
     else:
-        print("You have reached your destination your precious treasure island...")
+        print("You have reached your destination your precious treasure island...\n")
 
-    gate= input("There are 3 doors out of which only 1 leads to reach your treasury which door will you like to enter from? (RED/GREEN/BROWN): ").lower()
+    gate= input("There are 3 doors out of which only 1 leads to reach your treasury which door will you like to enter from? (RED/GREEN/BROWN): \n").lower()
 
     if gate== "red":
-        print("Hurryyyyyy!!! You reached to your treasure enjoy your wealth owned by a hardwork of yours!!")
+        print("Hurryyyyyy!!! You reached to your treasure enjoy your wealth owned by a hardwork of yours!!\n")
     else:
-        print("You entered the wrong gate filled up with lot of snakes...\n BETTER LUCK FOR NEXT TIME!!!")
+        print("You entered the wrong gate filled up with lot of snakes...\n BETTER LUCK FOR NEXT TIME!!!\n")
 
-print("Moral: Hard work and Luck both decide your future rely on your hardwork and let luck work when it wanted to..")
+print("Moral: Hard work and Luck both decide your future rely on your hardwork and let luck work when it wanted to..\n")
