0952bb3112f80ce8003b7973c89fe465a5fdc138
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-17 23:21:32 +0530

    Removed for loop to fix Type error

--- Guess_Number.py
+++ Guess_Number.py
@@ -8,32 +8,34 @@ def game():
     difficulty= input("Choose the difficulty (e.g: 'Easy/Hard'): ").lower()
     if difficulty=='easy':
         guesses= 10
-        game_control()
+        game_control(guesses)
     elif difficulty=='hard':
         guesses=5
-        game_control()
+        game_control(guesses)
     else:
         print("Enter valid input!!")
         
-def game_control():
-    print(f"You have total {guesses} guesses to identify the number")
-    guessed_number= int(input("Enter your number: "))
+def game_control(guesses):
         
     game_continue= True
+
     while game_continue and guesses>0:
-        for guessed_number in number_chosen:
-            if guessed_number== number_chosen:
-                print(f"You guessed correctly and won the game!!")
-            elif guessed_number!=number_chosen:
-                guesses-=1
-                if guessed_number>number_chosen:
-                    print(f"You guessed incorrectly and remaining chances of guessing is {guesses}")
-                elif guessed_number<number_chosen:
-                    print(f"You guessed incorrectly and remaining chances of guessing is {guesses}")
-                    print("Guessed number is too low than actual number")
-            else:
-                print("Enter the valid input")
-                game_continue= False
+
+        print(f"You have total {guesses} guesses to identify the number")
+        guessed_number= int(input("Enter your number: "))
+
+        if guessed_number== number_chosen:
+            print(f"You guessed correctly and won the game!!")
+        elif guessed_number!=number_chosen:
+            guesses-=1
+            if guessed_number>number_chosen:
+                print(f"You guessed incorrectly and remaining chances of guessing is {guesses}")
+            elif guessed_number<number_chosen:
+                print(f"You guessed incorrectly and remaining chances of guessing is {guesses}")
+                print("Guessed number is too low than actual number")
+        else:
+            print("Enter the valid input")
+            game_continue= False
 
         choice= input("To continue the game enter 'y' for yes and 'n' for no: ")
         if choice=='n':
