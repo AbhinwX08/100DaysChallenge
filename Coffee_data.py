5334281346385239d49deaf0aff3efbe5c5cb4ce
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-20 00:05:27 +0530

    saved datas of coffee and logo

---
+++ Coffee_data.py
@@ -0,0 +1,20 @@
+logo= (r'''
+     c  c     .-.                                 \\\    ///          c  c \\  // wW  Ww\\\  ///       
+     (OO)   c(O_O)c    wWw   wWw   wWw   wWw      ((O)  (O))   /)     (OO) (o)(o) (O)(O)((O)(O)) wWw   
+   ,'.--.) ,'.---.`,   (O)_  (O)_  (O)_  (O)_      | \  / |  (o)(O) ,'.--.)||  ||  (..)  | \ ||  (O)_  
+  / //_|_\/ /|_|_|\ \ .' __).' __).' __).' __)     ||\\//||   //\\ / //_|_\|(__)|   ||   ||\\|| .' __) 
+  | \___  | \_____/ |(  _) (  _) (  _) (  _)       || \/ ||  |(__)|| \___  /.--.\  _||_  || \ |(  _)   
+  '.    ) '. `---' .` )/    )/    `.__) `.__)      ||    ||  /,-. |'.    )-'    `-(_/\_) ||  || `.__)  
+    `-.'    `-...-'  (     (                      (_/    \_)-'   ''  `-.'               (_/  \_)       
+
+    ''')
+
+Coffee_ingredients= [
+    {'Name': 'Espresso', 'Milk (in ml)': 0, 'Coffee (in gm)': 18, 'Water (in ml)': 60,},
+    {'Name': 'Latte', 'Milk (in ml)': 240, 'Coffee (in gm)': 18, 'Water (in ml)': 60,},
+    {'Name': 'Cappuccino', 'Milk (in ml)': 120, 'Coffee (in gm)': 18, 'Water (in ml)': 60,}
+]
+
+Machine= {'Milk (in ml)': 300, 'Coffee (in gm)': 100, 'Water (in ml)': 500}
+
+Types= {'Espresso (₹)': 150, 'Latte (₹)': 250, 'Cappuccino (₹)': 300}
