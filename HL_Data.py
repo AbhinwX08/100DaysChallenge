1c7486daafa5b4f7d609ceab9014287cc864b118
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-19 10:35:01 +0530

    Used list for storing data

---
+++ HL_Data.py
@@ -0,0 +1,60 @@
+logo_1= (r'''  o         o     o                 o                                                                                        
+ <|>       <|>  _<|>_              <|>                                                                                       
+ < >       < >                     / >                                                                                       
+  |         |     o      o__ __o/  \o__ __o      o__  __o   \o__ __o                                                         
+  o__/_ _\__o    <|>    /v     |    |     v\    /v      |>   |     |>                                                        
+  |         |    / \   />     / \  / \     <\  />      //   / \   < >                                                        
+ <o>       <o>   \o/   \      \o/  \o/     o/  \o    o/     \o/                                                              
+  |         |     |     o      |    |     <|    v\  /v __o   |                                                               
+ / \       / \   / \    <\__  < >  / \    / \    <\/> __/>  / \                                                              
+                               |                                                                                             
+                       o__     o                                                                                             
+                       <\__ __/>                                                                                             
+                                                          o                                                                  
+                                                         <|>                                                                 
+                                                         / \                                                                 
+                                                         \o/           o__ __o     o              o     o__  __o   \o__ __o  
+                                                          |           /v     v\   <|>            <|>   /v      |>   |     |> 
+                                                         / \         />       <\  < >            < >  />      //   / \   < > 
+                                                         \o/         \         /   \o    o/\o    o/   \o    o/     \o/       
+                                                          |           o       o     v\  /v  v\  /v     v\  /v __o   |        
+                                                         / \ _\o__/_  <\__ __/>      <\/>    <\/>       <\/> __/>  / \       
+                                                                                                                           
+''')
+
+logo_2= (r'''
+ __      __   ____     
+ \ \   /"/u / __"| u  
+  \ \ / // <\___ \/   
+  /\ V /_,-.u___) |   
+ U  \_/-(_/ |____/>>  
+   //        )(  (__) 
+  (__)      (__)      
+         
+''')
+
+
+
+data_of_personalities= [
+    {"Name": "Virat Kohli", "age": 37, "country": "India", "followers_millions": 274},
+    {"Name": "Shraddha Kapoor", "age": 39, "country": "India", "followers_millions": 96},
+    {"Name": "Priyanka Chopra", "age": 44, "country": "India", "followers_millions": 93},
+    {"Name": "Alia Bhatt", "age": 33, "country": "India", "followers_millions": 87},
+    {"Name": "Deepika Padukone", "age": 40, "country": "India", "followers_millions": 81},
+    {"Name": "Katrina Kaif", "age": 43, "country": "India", "followers_millions": 81},
+    {"Name": "Salman Khan", "age": 60, "country": "India", "followers_millions": 71},
+    {"Name": "Shah Rukh Khan", "age": 60, "country": "India", "followers_millions": 49},
+    {"Name": "MS Dhoni", "age": 45, "country": "India", "followers_millions": 50},
+    {"Name": "Rashmika Mandanna", "age": 30, "country": "India", "followers_millions": 52},
+    {"Name": "Ranveer Singh", "age": 41, "country": "India", "followers_millions": 48},
+    {"Name": "Akshay Kumar", "age": 59, "country": "India", "followers_millions": 68},
+    {"Name": "Amitabh Bachchan", "age": 84, "country": "India", "followers_millions": 38},
+    {"Name": "Anushka Sharma", "age": 38, "country": "India", "followers_millions": 69},
+    {"Name": "Kiara Advani", "age": 35, "country": "India", "followers_millions": 36},
+    {"Name": "Kriti Sanon", "age": 36, "country": "India", "followers_millions": 59},
+    {"Name": "Sara Ali Khan", "age": 31, "country": "India", "followers_millions": 46},
+    {"Name": "Janhvi Kapoor", "age": 29, "country": "India", "followers_millions": 28},
+    {"Name": "Kareena Kapoor", "age": 46, "country": "India", "followers_millions": 13},
+    {"Name": "Aishwarya Rai Bachchan", "age": 53, "country": "India", "followers_millions": 15},
+    {"Name": "Hrithik Roshan", "age": 52, "country": "India", "followers_millions": 49},
+]
