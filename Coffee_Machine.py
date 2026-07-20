84832f0d9755dbc15f8153046ee4d3e7a5e01e44
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-20 07:05:16 +0530

    Used f string

--- Coffee_Machine.py
+++ Coffee_Machine.py
@@ -5,11 +5,13 @@ print("\nWelcome to the world of making coffee!!")
 
 Machine_report= input("Before making coffee do you want to see the available ingredients present in the machine?\nEnter 'y' for yes and 'n' for no: ").lower()
 if Machine_report=='y':
-    print(Coffee_data.Machine)
+    print (f"Milk (in ml): {Coffee_data.Machine['Milk']},Cofee (in gm): {Coffee_data.Machine['Coffee']} and Water (in ml): {Coffee_data.Machine['Water']}")
 elif Machine_report=='n':
     print("Continue making your coffee!!")
 else:
     print("Enter valid input!!")
 
+print("We have all these types of coffee with their prices, Please select anyone type from the list given below :)")
 
-print(f"We have all these types of coffee please select anyone type from the list given below :)\n{Coffee_data.Types}")
+for key in Coffee_data.Types:
+    print(f"Coffee type: {key} and its Price (in ₹): {Coffee_data.Types[key]}")
