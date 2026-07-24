9ddc21a998f7be232daebaad4f1e200295064ef0
AbhinwX08 <abhinwu0807@gmail.com>
2026-07-24 09:57:54 +0530

    removed print function

--- Question_Model.py
+++ Question_Model.py
@@ -2,9 +2,3 @@ class Question:
     def __init__(self,text,answer):
         self.text= text
         self.answer= answer
-
-Question_1= Question('What is your score: Abhinav','100')
-Question_2= Question('What is your score: Bhavya','200')
-
-print(Question_1)
-print(Question_2)
